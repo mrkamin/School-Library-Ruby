e1bdd07ebf27bc9b13496db405dcf068d17c2d6c
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-04-27 01:28:09 +0500

    Set Up Associations

--- person.rb
+++ person.rb
@@ -1,11 +1,15 @@
 require './nameable'
 
 class Person < Nameable
+  attr_accessor :name, :age
+  attr_reader :id, :rentals
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
     super()
   end
 
@@ -13,9 +17,9 @@ class Person < Nameable
     @name
   end
 
-  attr_accessor :name, :age
-
-  attr_reader :id
+  def add_rental(book, date)
+    Rental.new(date, self, book)
+  end
 
   def can_use_services?
     of_age? || @parent_permission
