4abe34b168cef5e231ea5174f6bfe4703188aa45
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-04-27 01:33:11 +0500

    Fixing the Linters

--- rental.rb
+++ rental.rb
@@ -1,12 +1,12 @@
 class Rental
-    attr_accessor  :person, :book, :date
+  attr_accessor :person, :book, :date
 
-    def initialize(date, person, book)
-        @date = date
-        @person = person
-        person.rentals << self
+  def initialize(date, person, book)
+    @date = date
+    @person = person
+    person.rentals << self
 
-        @book = book
-        book.rentals << self 
-    end
+    @book = book
+    book.rentals << self
+  end
 end
--- student.rb
+++ student.rb
@@ -12,7 +12,7 @@ class Student < Person
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
-  
+
   def play_hooky
     '¯(ツ)/¯'
   end
