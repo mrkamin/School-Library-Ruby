b60cecf358e7d4682fb8d169c3e399429abac1ad
Mohammad Rafi Amin <mrkamin2@gmail.com>
2023-04-28 05:46:05 +0500

    Apply Reviewer feedback

--- book.rb
+++ book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :title, :author
-  attr_reader :rentals
+  attr_accessor :title, :author, :rentals
+
 
   def initialize(author, title)
     @title = title
--- person.rb
+++ person.rb
@@ -1,15 +1,15 @@
 require './nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age
-  attr_reader :id, :rentals
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @rental = []
+    @rentals = []
     super()
   end
 
